ba6a42e8f4c76f141ee510296ca35df0ec51518e
pgv3728 <ext.solutec.abourdarel@grandlyon.com>
2024-02-21 17:42:45 +0100

    wip

--- bash/v_synchro_rsync.sh
+++ bash/v_synchro_rsync.sh
@@ -1,55 +1,44 @@
 #!/bin/bash
 
-##  Programme    : v_synchro_rsync.sh
+##  Programme    : v_rsync_synchro.sh
 ##  Auteur       : BOURDAREL Antoine (TME Solutec)
-##  Date         : 16/02/2024
+##  Date         : 21/02/2024
 ##
-##  Description  :  Synchronisation de fichiers via RSYNC
+##  Description  : Copie des fichiers d'un serveur windows à un serveur linux
 ##
-##  Paramètres   : 
-##          1 - Chemin du répertoire source Windows (en notation Linux - ex: c/TEMP/temp pour C:\TEMP\temp)
-##          2 - Chemin du répertoire cible Linux (en notation Linux - ex : /opt/gl/merco)
-##          3 : Nom du serveur cible linux (nom complet du serveur - ex:l-grc-as-r01.grandlyon.fr)
-##          4 : Compte de connexion au serveur linux
-##          5 : Suppression du fichier source ('O' ou 'N')
-##          6..n : Nom du répertoire à copier
+##  Paramètres   : aucun
 ##
-##  Retour       : 
-##          0       Fin normale de la copie des fichiers 
-##          1       En cas d'erreur
-##          2       Nombre de paramètres recus incorrect
-##          3       Erreur dans la copie d'un fichier vers le repertoire cible
+##  Retour       : 1 en cas d'erreur
+##                 0 sinon
+##                 8 paramètres incorrect
 ##
 ##  Modifications ulterieures :
 ## Date        Par                             Objet
 ## ----------- ------------------------------- ----------------------------------------------------------------------------------
 ##
+##
+##
 
-# CONFIGURATION INITIALE
-# [[ Vérification paramètres ]]
+#--
+#-- Verifications
+#--
 
-if [[ $# -ne 5 ]]; then
-    print " Erreur - Nombre de paramètres incorrects"
-    exit 2
+if [[ $# -ne 3 ]]; then
+    print "ERREUR ERREUR ERREUR ERREUR"
+    print "Le nombre de paramètres est incorrect. Paramètres attendus : 3"
+    print "ERREUR ERREUR ERREUR ERREUR"
+    exit 8
 fi
 
-# [[ Mise en place des variables ]]
-rep_source=$1
-rep_cible=$2
-srv_cible=$3
-compte=$4
-repertoire=$5
-chemin_source="/cygdrive/$rep_source"
+chemin_source=$1
+chemin_cible=$2
+password_file=$3
 
-print "==> Parametre 1 : Nom du répertoire source"
-print "                    $rep_source"
-print "==> Parametre 2 : Nom du répertoire cible"
-print "                    $rep_cible"
-print "==> Parametre 3 : Nom du serveur cible"
-print "                    $srv_cible"
-print "==> Parametre 4 : Nom du compte"
-print "                    $compte"
-print "==> Parametre 5 : Nom du chemin source"
-print "                    $chemin_source"
-print
+echo "===================="
+echo "Chemin source : $chemin_source"
+echo "Chemin cible : $chemin_cible"
+echo "password : $password_file"
+echo "===================="
 
+
+rsync -avz --password-file=$password_file $chemin_source $chemin_cible
